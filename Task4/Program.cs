04f712b89666bf03e3979d83a5c6275188d8601b
Naufal <nauf1207@mail.ru>
2023-03-09 02:41:09 +0600

    done task 4

---
+++ Task4/Program.cs
@@ -0,0 +1,21 @@
+﻿int max = 0;
+
+Console.Write("Введите первое число: ");
+int a = Convert.ToInt32(Console.ReadLine());
+
+Console.Write("Введите второе число: ");
+int b = Convert.ToInt32(Console.ReadLine());
+
+Console.Write("Введите третье число: ");
+int c = Convert.ToInt32(Console.ReadLine());
+
+if (a > max)
+    max=a;
+
+if (b > max)
+    max=b;
+  
+if (c > max)
+    max=c;
+
+Console.WriteLine(max =  + max);
