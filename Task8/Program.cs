4fec4a6a0f3d038d2a60f13ded5bcf9bfc810598
Naufal <nauf1207@mail.ru>
2023-03-09 15:51:27 +0600

    done task 8

---
+++ Task8/Program.cs
@@ -0,0 +1,9 @@
+﻿Console.Clear();
+Console.Write("Введите число: ");
+int n = Convert.ToInt32(Console.ReadLine());
+
+for (int i = 2; i <= n; i+=2)
+{
+    Console.Write($"{i} ");
+}
+ Console.WriteLine();   
