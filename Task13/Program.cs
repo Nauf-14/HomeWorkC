929f190e3bc7ea2f2c4ecaaa738a4457c35fec3e
Naufal <nauf1207@mail.ru>
2023-03-10 22:06:10 +0600

    done task 13

---
+++ Task13/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.Clear();
+Console.Write("Введите число: ");
+int a = Convert.ToInt32(Console.ReadLine());
+if (a >= 100)
+{
+while  (a > 999)
+  a = a / 10;
+Console.WriteLine(a % 10);
+}
+if (a < 100)
+Console.WriteLine("третьей цифры нет");
+
